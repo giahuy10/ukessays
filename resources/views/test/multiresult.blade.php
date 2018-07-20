3ea67bbcd0cd7a2e5c015b36066263dcaa9ad9cf
Eddy Nguyen <eddynguyen@Eddys-Air.net.fpt>
2018-07-21 01:06:09 +0700

    update

--- resources/views/test/multiresult.blade.php
+++ resources/views/test/multiresult.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     @if ($correct >= 11)
+    <p></p><p></p>
         <div class="alert alert-success">
             <p>You passed {{$correct}} questions. You can continue to do <a href="{{route('test.topic')}}">Essay Topic Test</a> to complete registration.</p>
             <p>Essay will take 40 minutes to write a 500 words based on random topic.</p>
--- resources/views/test/topicresult.blade.php
+++ resources/views/test/topicresult.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 <div class="container">
+        <p></p>
+        <p></p>
     <p>We received your test. We will check and response as soon as possible. In the meantime, please fill data in your profile <a href="{{route('writer.updateprofile')}}">here</a>. </p>
 </div>
 @endsection 
